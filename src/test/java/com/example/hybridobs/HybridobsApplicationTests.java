3824960fda830ae3895b67141dfd94c8bbe3f23c
Raj Shivaram <raj.shivaram@hotmail.com>
2024-06-13 21:35:27 +0530

    Initial Commit
    Initial Commit

---
+++ src/test/java/com/example/hybridobs/HybridobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hybridobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
