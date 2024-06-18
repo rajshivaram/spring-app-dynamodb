ad9ce72be56ad1306d9b1084ad13961ecbec054e
Raj Shivaram <raj.shivaram@hotmail.com>
2024-06-18 19:15:11 +0530

    Updated logging

---
+++ src/main/java/com/crudDynamo/CrudDynamo/config/MetricsConfig.java
@@ -0,0 +1,45 @@
+package com.crudDynamo.CrudDynamo.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "spring-app");
+    }
+
+    @Bean
+    public ClassLoaderMetrics classLoaderMetrics() {
+        return new ClassLoaderMetrics();
+    }
+
+    @Bean
+    public JvmMemoryMetrics jvmMemoryMetrics() {
+        return new JvmMemoryMetrics();
+    }
+
+    @Bean
+    public JvmThreadMetrics jvmThreadMetrics() {
+        return new JvmThreadMetrics();
+    }
+
+    @Bean
+    public ProcessorMetrics processorMetrics() {
+        return new ProcessorMetrics();
+    }
+
+    @Bean
+    public LogbackMetrics logbackMetrics() {
+        return new LogbackMetrics();
+    }
+}
---
+++ src/main/java/com/crudDynamo/CrudDynamo/config/OpenTelemetryConfig.java
@@ -0,0 +1,33 @@
+package com.crudDynamo.CrudDynamo.config;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public Tracer tracer() {
+        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+                .setEndpoint("http://localhost:4317")
+                .build();
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .build();
+
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .build();
+
+        GlobalOpenTelemetry.set(openTelemetrySdk);
+
+        return openTelemetrySdk.getTracer("spring-app");
+    }
+}
